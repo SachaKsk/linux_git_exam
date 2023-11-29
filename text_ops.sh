87a8d7e817f0f5ff0c404c27450d03dfd24d7144
Sacha Koskas <sachakoskas8@gmail.com>
2023-11-29 09:42:18 +0100

    Script to find and sort numerical values from README.md

--- text_ops.sh
+++ text_ops.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 grep -o "function" README.md | wc -l > output/function-keyword-count.txt
 grep '^#' README.md | sed 's/^#* //' | sort | uniq > output/unique-headers.txt
+grep -o '[0-9]\+' README.md | sort -n > output/sorted-numbers.txt
