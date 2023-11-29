e886940c8caee07e5306eb04b57610aedd94e020
Sacha Koskas <sachakoskas8@gmail.com>
2023-11-29 09:45:50 +0100

    Added feature to concatenate FEATURE section lines

--- text_ops.sh
+++ text_ops.sh
@@ -2,3 +2,5 @@
 grep -o "function" README.md | wc -l > output/function-keyword-count.txt
 grep '^#' README.md | sed 's/^#* //' | sort | uniq > output/unique-headers.txt
 grep -o '[0-9]\+' README.md | sort -n > output/sorted-numbers.txt
+sed -n '/^# FEATURES/,/^#/p' README.md | grep -v '^#' | tr '
+' ' ' > output/features-summary.txt
